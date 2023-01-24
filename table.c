70009fcaa56abfd4a3003f00ae918d715d817bd9
Daniel Bendix <daniel@bendix.io>
2023-01-24 13:07:55 +0100

    Replace modulo with bitwise and in hash table.

--- table.c
+++ table.c
@@ -23,7 +23,7 @@ void freeTable(Table *table)
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
 {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry *tombstone = NULL;
 
     for (;;) {
@@ -42,7 +42,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -124,7 +124,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
 {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry *entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -135,7 +135,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
