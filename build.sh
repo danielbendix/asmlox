f03c9fc184f518bb8f96634190eb05d00020db94
Daniel Bendix <daniel@bendix.io>
2024-05-23 13:27:29 +0200

    Update build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,34 @@
-#!/bin/zsh
+#!/bin/bash
+
+symbols_flag=""
+optimization_flag="-O3"
+profiler_flag=""
+
+for arg in "$@"
+do
+    case $arg in
+        --debug)
+            symbols_flag="-g"
+            optimization_flag="-O0"
+        ;;
+        --profile)
+            symbols_flag="-g"
+            profiler_flag="-lprofiler"
+        ;;
+        -h|--help)
+        echo "Usage: $0 [options]"
+        echo "Options:"
+        echo "  -h, --help      Display this help message"
+        echo "  --debug         Create a debug build"
+        echo "  --profile       Link with the gperftools profiler library"
+        exit 0
+        ;;
+        *)
+        echo "Unknown option: $arg"
+        exit 1
+        ;;
+    esac
+done
 
 clang *.c \
 native/arm64.c \
@@ -15,5 +45,7 @@ native/arm64/global.o \
 native/arm64/closure.o \
 native/arm64/call.o \
 native/arm64/return.o \
--lprofiler \
--lreadline -O0 -g -o asmlox
+$symbols_flag \
+$optimization_flag \
+$profiler_flag \
+-lreadline -o asmlox
